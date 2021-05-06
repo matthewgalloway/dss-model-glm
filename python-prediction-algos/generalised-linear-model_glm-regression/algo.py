d9b71eb4666f8f8a8582707deb666ab9d02377f4
admin <admin>
2021-05-06 09:40:30 +0100

    Edited files

--- python-prediction-algos/generalised-linear-model_glm-regression/algo.py
+++ python-prediction-algos/generalised-linear-model_glm-regression/algo.py
@@ -1,35 +1,20 @@
-# This file is the actual code for the custom Python algorithm generalised-linear-model_glm-regression
+# This file is the actual code for the custom Python algorithm dev-glm_glm
 from dataiku.doctor.plugins.custom_prediction_algorithm import BaseCustomPredictionAlgorithm
-from sklearn.ensemble import AdaBoostRegressor
+from generalisedlinearmodel.dku_glm import RegressionGLM
 
 class CustomPredictionAlgorithm(BaseCustomPredictionAlgorithm):    
-    """
-        Class defining the behaviour of `generalised-linear-model_glm-regression` algorithm:
-        - how it handles parameters passed to it
-        - how the estimator works
-
-        Example here defines an Adaboost Regressor from Scikit Learn that would work for regression
-        (see https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostRegressor.html)
-
-        You need to at least define a `get_clf` method that must return a scikit-learn compatible model
-
-        Args:
-            prediction_type (str): type of prediction for which the algorithm is used. Is relevant when 
-                                   algorithm works for more than one type of prediction.
-                                   Possible values are: "BINARY_CLASSIFICATION", "MULTICLASS", "REGRESSION"
-            params (dict): dictionary of params set by the user in the UI.
-    """
     
-    def __init__(self, prediction_type=None, params=None):        
-        self.clf = AdaBoostRegressor(random_state=params.get("random_state", None))
-        super(CustomPredictionAlgorithm, self).__init__(prediction_type, params)
-    
+    def __init__(self, prediction_type=None, params=None):   
+        
+        self.params = params
+        self.clf = RegressionGLM(
+            link=params.get("link"),
+            family=params.get("family"),
+            alpha=params.get("alpha"),
+            power=params.get("power")
+        )
+        super(CustomPredictionAlgorithm, self).__init__(prediction_type, self.params)
+                                     
     def get_clf(self):
-        """
-        This method must return a scikit-learn compatible model, ie:
-        - have a fit(X,y) and predict(X) methods. If sample weights
-          are enabled for this algorithm (in algo.json), the fit method
-          must have instead the signature fit(X, y, sample_weight=None)
-        - have a get_params() and set_params(**params) methods
-        """
+
         return self.clf
